db0829a181d10884f19ad9cff9862f31c6970b2e
Hans klabbers <hans.klabbers@rabobank.nl>
2019-01-08 06:39:14 +0100

    Changed command to update /etc/environment and /etc/profile.

--- install_on_vagrant.sh
+++ install_on_vagrant.sh
@@ -2,22 +2,24 @@
 set -e
 locale-gen "en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
-cat <<EOT > /etc/environment
+cat <<EOT >> /etc/environment
 LC_ALL="en_US.UTF-8"
 EOT
 cd /IncludeOS
-echo "Y" | ./install.sh
+export INCLUDEOS_ENABLE_TEST=ON
 export INCLUDEOS_PREFIX=/usr/local
 export PATH=$PATH:$INCLUDEOS_PREFIX/includeos/bin
 export CC="clang-5.0"
 export CXX="clang++-5.0"
-cat <<EOT > /etc/environment
+echo "Y" | ./install.sh
+cat <<EOT >> /etc/environment
 INCLUDEOS_PREFIX=/usr/local
 CC="clang-5.0"
 CXX="clang++-5.0"
 EOT
-
-cat <<EOT > /etc/profile
+cat <<EOT >> /etc/profile
 
 PATH=$PATH:$INCLUDEOS_PREFIX/includeos/bin
 EOT
+pip install --upgrade pip
+pip install subprocess32
